2dc2853ae2ddc9479033781e42a11a14fb36cf9e
jamie <jamie@email>
2023-09-27 13:57:46 +0100

    JM added text lines

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'This is the about page'
+    return 'This is the most amazing about page EVER!!!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
